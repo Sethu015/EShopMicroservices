65f2a3aa29786a527a1eddc9acc504cd95105101
Sethu015 <sethuram015@gmail.com>
2025-05-10 11:47:00 +0530

    Repo pattern completed in basket

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -1,9 +1,35 @@
+using Basket.API;
+using Basket.API.Basket.DeleteBasket;
+
 var builder = WebApplication.CreateBuilder(args);
 
+var assembly = typeof(Program).Assembly;
+
 //Adding Services
+
+//Add Carter
+builder.Services.AddCarter(new DependencyContextAssemblyCatalogCustom());
+
+//Add Mediatr
+builder.Services.AddMediatR(config =>
+{
+    config.RegisterServicesFromAssembly(assembly);
+    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+});
+
+//Add fluent validation
+builder.Services.AddValidatorsFromAssembly(assembly);
+
+//Mapster Configs
+TypeAdapterConfig.GlobalSettings.Default.NameMatchingStrategy(NameMatchingStrategy.IgnoreCase);
+TypeAdapterConfig.GlobalSettings.NewConfig<DeleteBasketResult, DeleteBasketResponse>().Map(dest => dest.isSuccess, src => src.isSuccess);
+
 
 var app = builder.Build();
 
 //Configuring request pipeline
+
+app.MapCarter();
 
 app.Run();
