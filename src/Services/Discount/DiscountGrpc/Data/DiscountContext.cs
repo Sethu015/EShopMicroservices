4354f8dd4a788cb3196c7e0f87ee209e07fcc436
Sethu015 <sethuram015@gmail.com>
2025-06-04 09:40:46 +0530

    Migration added and Db created

---
+++ src/Services/Discount/DiscountGrpc/Data/DiscountContext.cs
@@ -0,0 +1,16 @@
+﻿using DiscountGrpc.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DiscountGrpc.Data
+{
+    public class DiscountContext : DbContext
+    {
+        public DbSet<Coupon> Coupones { get; set; } = default!;
+
+        public DiscountContext(DbContextOptions<DiscountContext> options)
+            : base(options)
+        {
+            
+        }
+    }
+}
--- src/Services/Discount/DiscountGrpc/Program.cs
+++ src/Services/Discount/DiscountGrpc/Program.cs
@@ -1,4 +1,6 @@
+using DiscountGrpc.Data;
 using DiscountGrpc.Services;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,10 +9,16 @@ builder.Services.AddGrpc();
 
 builder.Services.AddGrpcReflection();
 
+builder.Services.AddDbContext<DiscountContext>(opts =>
+{
+    opts.UseSqlite(builder.Configuration.GetConnectionString("Database"));
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 app.MapGrpcService<GreeterService>();
+app.MapGrpcService<DiscountService>();
 app.MapGet("/", () => "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
 
 if (app.Environment.IsDevelopment())
