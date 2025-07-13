db5b6d77fb6a290e15a5abaf8f32d093619ec8cc
Sethu015 <sethuram015@gmail.com>
2025-07-13 15:33:59 +0530

    Added Ordering Project

---
+++ src/Services/Ordering/Ordering.API/DependencyInjection.cs
@@ -0,0 +1,17 @@
+﻿namespace Ordering.API
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddApiServices(this IServiceCollection services)
+        {
+            //Add Web related Services
+            return services;
+        }
+
+        public static WebApplication UseApiServices(this WebApplication webApplication)
+        {
+            //Add Middlewares
+            return webApplication;
+        }
+    }
+}
--- src/Services/Ordering/Ordering.API/Program.cs
+++ src/Services/Ordering/Ordering.API/Program.cs
@@ -1,9 +1,20 @@
+using Ordering.API;
+using Ordering.Application;
+using Ordering.Infrastructure;
+
 var builder = WebApplication.CreateBuilder(args);
 
 //Add Services for Dependency Injection
 
+builder.Services
+    .AddApplicationServices()
+    .AddInfrastructureServices(builder.Configuration)
+    .AddApiServices();
+
 var app = builder.Build();
 
 //Add to Http Pipeline
+
+app.UseApiServices();
 
 app.Run();
