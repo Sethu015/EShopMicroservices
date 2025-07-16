908fa5796f3f0163590fcd2949557986a701f001
Sethu015 <sethuram015@gmail.com>
2025-07-16 10:06:23 +0530

    Aggregates added

---
+++ src/Services/Ordering/Ordering.Domain/Abstractions/Aggregate.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Ordering.Domain.Abstractions
+{
+    public abstract class Aggregate<TId> : Entity<TId>, IAggregate<TId>
+    {
+        private readonly List<IDomainEvent> _events = new();
+        public IReadOnlyList<IDomainEvent> DomainEvents => _events.AsReadOnly();
+
+        public void AddDomainEvent(IDomainEvent domainEvent)
+        {
+            _events.Add(domainEvent);
+        }
+
+        public IDomainEvent[] ClearDomainEvents()
+        {
+            IDomainEvent[] events = _events.ToArray();
+            _events.Clear();
+            return events;
+        }
+    }
+}
