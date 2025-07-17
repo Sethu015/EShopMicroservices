a77ea89426fae7cf3af9f8cdf8443da07ae57242
Sethu015 <sethuram015@gmail.com>
2025-07-17 10:10:42 +0530

    Added domain models Ordering

---
+++ src/Services/Ordering/Ordering.Domain/Models/OrderItem.cs
@@ -0,0 +1,21 @@
+﻿using Ordering.Domain.Abstractions;
+
+namespace Ordering.Domain.Models
+{
+    public class OrderItem : Entity<Guid>
+    {
+        internal OrderItem(Guid orderId,Guid productId,int quantity,decimal totalPrice)
+        {
+            OrderId = orderId;
+            ProductId = productId;
+            Quantity = quantity;
+            TotalPrice = totalPrice;
+        }
+
+        public Guid OrderId { get; private set; } = default!;
+        public Guid ProductId { get; private set; } = default!;
+        public int Quantity { get; private set; } = default!;
+        public decimal TotalPrice { get; }
+        public decimal Price { get; private set;} = default!;
+    }
+}
