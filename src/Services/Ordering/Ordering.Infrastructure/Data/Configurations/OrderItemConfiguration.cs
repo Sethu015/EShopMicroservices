accd935340abd1617f28bb14e7a657df3e51350c
Sethu015 <sethuram015@gmail.com>
2025-07-27 19:09:04 +0530

    Changes

---
+++ src/Services/Ordering/Ordering.Infrastructure/Data/Configurations/OrderItemConfiguration.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Ordering.Infrastructure.Data.Configurations
+{
+    public class OrderItemConfiguration : IEntityTypeConfiguration<OrderItem>
+    {
+        public void Configure(EntityTypeBuilder<OrderItem> builder)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
