330e2a1c18ec1a2d9a5374eb98040426454b3a00
Sethu015 <sethuram015@gmail.com>
2025-08-24 21:48:44 +0530

    Delete Order

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/DeleteOrder/DeleteOrderCommandHandler.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Ordering.Application.Exception;
+
+namespace Ordering.Application.Orders.Commands.DeleteOrder
+{
+    public class DeleteOrderCommandHandler(IApplicationDbContext context)
+        : ICommandHandler<DeleteOrderCommand, DeleteOrderResult>
+    {
+        public async Task<DeleteOrderResult> Handle(DeleteOrderCommand command, CancellationToken cancellationToken)
+        {
+            var order = await context.Orders
+                .FindAsync([OrderId.Of(command.Id)], cancellationToken);
+
+            if(order is null)
+                throw new OrderNotFoundException(command.Id);
+
+            context.Orders.Remove(order);
+            await context.SaveChangesAsync(cancellationToken);
+            return new DeleteOrderResult(true);
+        }
+    }
+}
