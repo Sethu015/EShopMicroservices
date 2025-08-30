cdcbdf2616cf9b1686ba2b1dca96cd970818a856
Sethu015 <sethuram015@gmail.com>
2025-08-30 20:05:14 +0530

    Changes

---
+++ src/Services/Ordering/Ordering.Application/Extensions/OrderExtensions.cs
@@ -0,0 +1,19 @@
+﻿namespace Ordering.Application.Extensions
+{
+    public static class OrderExtensions
+    {
+        public static IEnumerable<OrderDto> ToOrderDtoList(this IEnumerable<Order> orders)
+        {
+            return orders.Select(o => new OrderDto(
+                o.Id.Value,
+                o.CustomerId.Value,
+                o.OrderName.Value,
+                new AddressDto(o.ShippingAddress.FirstName, o.ShippingAddress.LastName, o.ShippingAddress.EmailAddress, o.ShippingAddress.AddressLine, o.ShippingAddress.Country, o.ShippingAddress.State, o.ShippingAddress.Zip),
+                new AddressDto(o.BillingAddress.FirstName, o.BillingAddress.LastName, o.BillingAddress.EmailAddress, o.BillingAddress.AddressLine, o.BillingAddress.Country, o.BillingAddress.State, o.BillingAddress.Zip),
+                new PaymentDto(o.Payment.CardName, o.Payment.CardNumber, o.Payment.Expiration, o.Payment.CVV, o.Payment.PaymentMethod),
+                o.OrderStatus,
+                o.OrderItems.Select(oi => new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList()
+                ));
+        }
+    }
+}
--- src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQueryHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Queries/GetOrdersByName/GetOrdersByNameQueryHandler.cs
@@ -1,4 +1,6 @@
-﻿namespace Ordering.Application.Orders.Queries.GetOrdersByName
+﻿using Ordering.Application.Extensions;
+
+namespace Ordering.Application.Orders.Queries.GetOrdersByName
 {
     public class GetOrdersByNameQueryHandler(IApplicationDbContext context)
         : IQueryHandler<GetOrdersByNameQuery, GetOrdersByNameResult>
@@ -10,42 +12,42 @@
                          .Where(o => o.OrderName.Value.Contains(request.OrderName,StringComparison.OrdinalIgnoreCase))
                          .OrderBy(o => o.OrderName)
                          .ToListAsync(cancellationToken);
-            var orderDtos = ProjectToOrderDto(orders);
-            return new GetOrdersByNameResult(orderDtos);
+            //var orderDtos = ProjectToOrderDto(orders);
+            return new GetOrdersByNameResult(orders.ToOrderDtoList());
         }
 
-        private List<OrderDto> ProjectToOrderDto(List<Order> orders)
-        {
-            var orderDtos = new List<OrderDto>();
-            foreach (var order in orders)
-            {
-                var orderDto = new OrderDto(order.Id.Value
-                    , order.CustomerId.Value,
-                    order.OrderName.Value,
-                    new AddressDto(order.ShippingAddress.FirstName,
-                    order.ShippingAddress.LastName,
-                    order.ShippingAddress.EmailAddress,
-                    order.ShippingAddress.AddressLine,
-                    order.ShippingAddress.Country,
-                    order.ShippingAddress.State,
-                    order.ShippingAddress.Zip),
-                    new AddressDto(order.BillingAddress.FirstName,
-                    order.BillingAddress.LastName,
-                    order.BillingAddress.EmailAddress,
-                    order.BillingAddress.AddressLine,
-                    order.BillingAddress.Country,
-                    order.BillingAddress.State,
-                    order.BillingAddress.Zip),
-                    new PaymentDto(order.Payment.CardName,
-                    order.Payment.CardNumber,
-                    order.Payment.Expiration,
-                    order.Payment.CVV,
-                    order.Payment.PaymentMethod),
-                    order.OrderStatus,
-                    order.OrderItems.Select(oi => new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList());
-                orderDtos.Add(orderDto);
-            }
-            return orderDtos;
-        }
+        //private List<OrderDto> ProjectToOrderDto(List<Order> orders)
+        //{
+        //    var orderDtos = new List<OrderDto>();
+        //    foreach (var order in orders)
+        //    {
+        //        var orderDto = new OrderDto(order.Id.Value
+        //            , order.CustomerId.Value,
+        //            order.OrderName.Value,
+        //            new AddressDto(order.ShippingAddress.FirstName,
+        //            order.ShippingAddress.LastName,
+        //            order.ShippingAddress.EmailAddress,
+        //            order.ShippingAddress.AddressLine,
+        //            order.ShippingAddress.Country,
+        //            order.ShippingAddress.State,
+        //            order.ShippingAddress.Zip),
+        //            new AddressDto(order.BillingAddress.FirstName,
+        //            order.BillingAddress.LastName,
+        //            order.BillingAddress.EmailAddress,
+        //            order.BillingAddress.AddressLine,
+        //            order.BillingAddress.Country,
+        //            order.BillingAddress.State,
+        //            order.BillingAddress.Zip),
+        //            new PaymentDto(order.Payment.CardName,
+        //            order.Payment.CardNumber,
+        //            order.Payment.Expiration,
+        //            order.Payment.CVV,
+        //            order.Payment.PaymentMethod),
+        //            order.OrderStatus,
+        //            order.OrderItems.Select(oi => new OrderItemDto(oi.OrderId.Value, oi.ProductId.Value, oi.Quantity, oi.Price)).ToList());
+        //        orderDtos.Add(orderDto);
+        //    }
+        //    return orderDtos;
+        //}
     }
 }
