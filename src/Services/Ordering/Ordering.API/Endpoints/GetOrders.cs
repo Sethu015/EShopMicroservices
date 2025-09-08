db42f247ebdf59f44621e613cee91f093a817bb4
Sethu015 <sethuram015@gmail.com>
2025-09-08 09:15:10 +0530

    API Layer completed

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,6 +1,25 @@
-﻿namespace Ordering.API.Endpoints
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints
 {
-    public class GetOrders
+    public record GetOrderResponse(PaginationResult<OrderDto> Orders);
+    public class GetOrders : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+            {
+                var query = new GetOrdersQuery(request);
+                var orders = await sender.Send(query);
+                var ordersDto = orders.Adapt<GetOrderResponse>();
+                return Results.Ok(ordersDto);
+            })
+            .WithName("GetOrders")
+            .WithDescription("Get Orders")
+            .WithSummary("Get Orders")
+            .Produces<GetOrderResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest);
+        }
     }
 }
